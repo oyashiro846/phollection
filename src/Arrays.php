e33cb2fa077c69d8c43380fb2a7ea5715ed0d9dd
Javakky <iemura.java@gmail.com>
2025-11-25 00:04:38 +0900

    feat: array_reduce のラッパー

--- src/Arrays.php
+++ src/Arrays.php
@@ -139,4 +139,28 @@ class Arrays
 
         return \array_slice($input, $offset, $length, $mode === Mode::MODE_ASSOC);
     }
+
+    /**
+     * 配列を畳み込んで単一の値にします.
+     *
+     * @template V
+     * @template R
+     *
+     * @param array<V> $input
+     * @param callable(R|V|null, V): (R|V|null) $callback
+     * @param R $initial
+     * @phpstan-param (R is null ?
+     *    callable(?V, V): ?V :
+     *    callable(R, V): R
+     *  ) $callback
+     * @return R|V|null
+     * @phpstan-return (R is null ? (?V) : R)
+     */
+    public static function reduce(array $input, callable $callback, mixed $initial = null): mixed
+    {
+        // $initial が null の場合、 R が登場しないので型パラメータは論理的に整合するが、
+        // PHPStan は関数内のチェックでは分岐をしてくれず、 callback(R|V, V): R|V を要求してしまう
+        // @phpstan-ignore argument.type
+        return array_reduce($input, $callback, $initial);
+    }
 }
